b940a8aac14e60b37472e0bc51cd37ee9ff1c88d
manimbbm <marqueisbruno@gmail.com>
2020-10-10 12:39:25 +0100

    Updated node packages and added hello world test file

---
+++ src/app/home-page/home-page.spec.ts
@@ -0,0 +1,8 @@
+import { DebugElement } from "@angular/core"
+import { TestBed } from '@angular/core/testing'
+
+describe('playing with Jasmine', () => {
+    it('should be true :)', () => {
+        expect(true).toBe(true)
+    })
+})
