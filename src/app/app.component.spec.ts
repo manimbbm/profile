3a3af62007f003b32a5264550f0cab5d79408036
manimbbm <marqueisbruno@gmail.com>
2020-10-10 12:25:00 +0100

    Fixed default basic tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,21 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
+import { SideNavComponent } from './shared/side-nav/side-nav.component';
+import { TopBarComponent } from './shared/top-bar/top-bar.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        FontAwesomeModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        SideNavComponent,
+        TopBarComponent
       ],
     }).compileComponents();
   }));
@@ -23,13 +29,13 @@ describe('AppComponent', () => {
   it(`should have as title 'app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('manimbbm');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('app app is running!');
+    expect(compiled.querySelector('footer p').textContent).toContain('Bruno Marques');
   });
 });
